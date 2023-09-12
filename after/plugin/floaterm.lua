d1dcd677e40bb8dd9b2ee1d3e0520b01c3b463d7
Shane Chesnutt <shanechesnutt@gmail.com>
2023-09-12 00:05:24 -0400

    Add descriptions to keymappings

--- after/plugin/floaterm.lua
+++ after/plugin/floaterm.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "<F12>", ":FloatermToggle<CR>")
+vim.keymap.set("n", "<F12>", ":FloatermToggle<CR>", { desc = "Toggle Floaterm" })
 vim.keymap.set("t", "<F12>", "<C-\\><C-n>:FloatermToggle<CR>")
--- after/plugin/indent-blankline.lua
+++ after/plugin/indent-blankline.lua
@@ -2,4 +2,4 @@ require("indent_blankline").setup {
     show_end_of_line = true,
 }
 
-vim.keymap.set("n", "<leader>ig", ":IndentBlanklineToggle!<CR>")
+vim.keymap.set("n", "<leader>ig", ":IndentBlanklineToggle!<CR>", { desc = "Toggle indent-blankline" })
