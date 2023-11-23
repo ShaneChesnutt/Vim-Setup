aef8e6d7034df5ce1b9643d2d1712179192e8621
Shane Chesnutt <shanechesnutt@gmail.com>
2023-11-23 01:00:02 -0500

    Upgrade telescope to version 0.1.4

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,7 +16,7 @@ vim.g.mapleader = ","
 
 local plugins = {
   {
-	  'nvim-telescope/telescope.nvim', version = '0.1.0',
+	  'nvim-telescope/telescope.nvim', version = '0.1.4',
 	  -- or                            , branch = '0.1.x',
 	  dependencies = { {'nvim-lua/plenary.nvim'} }
   },
