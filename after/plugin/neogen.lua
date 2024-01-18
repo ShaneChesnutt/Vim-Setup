47c79730119bfae790986f5bec5514cd1cda9dd6
Shane Chesnutt <shanechesnutt@gmail.com>
2024-01-18 16:25:33 -0500

    Add neogen keymapping

--- after/plugin/neogen.lua
+++ after/plugin/neogen.lua
@@ -1,7 +1,13 @@
-require('neogen').setup(
+local neogen = require('neogen')
+neogen.setup(
   {
     languages = {
       ['javascript.jsdoc'] = require('neogen.configurations.javascript')
     }
   }
+)
+
+vim.api.nvim_set_keymap(
+  'n', '<Leader>gd', ':lua require(\'neogen\').generate()<CR>',
+  { noremap = true, silent = true }
 )
