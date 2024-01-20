30ea1552b223447324837c93c181f22a4f433b92
Shane Chesnutt <shanechesnutt@gmail.com>
2024-01-20 14:48:57 -0500

    Add enable cursorline highlighting

--- lua/config/color_scheme.lua
+++ lua/config/color_scheme.lua
@@ -1,21 +1,22 @@
-require('onedarkpro').setup(
-  {
-    styles = {
-      types = 'NONE',
-      methods = 'NONE',
-      numbers = 'NONE',
-      strings = 'italic',
-      comments = 'italic',
-      keywords = 'bold,italic',
-      constants = 'NONE',
-      functions = 'italic',
-      operators = 'NONE',
-      variables = 'NONE',
-      parameters = 'NONE',
-      conditionals = 'italic',
-      virtual_text = 'NONE'
-    }
+require('onedarkpro').setup({
+  options = {
+    cursorline = true
+  },
+  styles = {
+    types = 'NONE',
+    methods = 'NONE',
+    numbers = 'NONE',
+    strings = 'italic',
+    comments = 'italic',
+    keywords = 'bold,italic',
+    constants = 'NONE',
+    functions = 'italic',
+    operators = 'NONE',
+    variables = 'NONE',
+    parameters = 'NONE',
+    conditionals = 'italic',
+    virtual_text = 'NONE'
   }
-)
+})
 
 vim.cmd('colorscheme onedark')
