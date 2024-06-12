a763bea37b9c7c10543d94dd2cddf54819a04c42
Shane Chesnutt <shanechesnutt@gmail.com>
2024-06-12 10:25:42 -0400

    Use lazy vim config for indent-blankline

--- lua/walnutt/plugins/indent-blankline.lua
+++ lua/walnutt/plugins/indent-blankline.lua
@@ -1,10 +1,26 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  main = "ibl",
   opts = {
-    scope = {
-      enabled = true,
+    indent = {
+      char = "│",
+      tab_char = "│",
+    },
+    scope = { show_start = false, show_end = false },
+    exclude = {
+      filetypes = {
+        "help",
+        "alpha",
+        "dashboard",
+        "neo-tree",
+        "Trouble",
+        "trouble",
+        "lazy",
+        "mason",
+        "notify",
+        "toggleterm",
+        "lazyterm",
+      },
     },
   },
+  main = "ibl",
 }
