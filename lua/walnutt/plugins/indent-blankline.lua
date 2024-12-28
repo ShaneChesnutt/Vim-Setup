ae71895355f10669d517650e173bf84d336868ba
Shane Chesnutt <shanechesnutt@gmail.com>
2024-12-28 17:16:36 -0500

    Remove custom indent line

--- lua/walnutt/plugins/indent-blankline.lua
+++ lua/walnutt/plugins/indent-blankline.lua
@@ -1,10 +1,6 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   opts = {
-    indent = {
-      char = "│",
-      tab_char = "│",
-    },
     scope = { show_start = false, show_end = false },
     exclude = {
       filetypes = {
