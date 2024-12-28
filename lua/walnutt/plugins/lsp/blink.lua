69c6f8e2a5a401eb594ed8ba6bcfd4ab7c8198b4
Shane Chesnutt <shanechesnutt@gmail.com>
2024-12-28 17:15:46 -0500

    Remove blink autocomplete in cmdline

--- lua/walnutt/plugins/lsp/blink.lua
+++ lua/walnutt/plugins/lsp/blink.lua
@@ -23,7 +23,7 @@ return {
       -- Sets the fallback highlight groups to nvim-cmp's highlight groups
       -- Useful for when your theme doesn't support blink.cmp
       -- Will be removed in a future release
-      use_nvim_cmp_as_default = true,
+      use_nvim_cmp_as_default = false,
       -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = "mono",
@@ -33,7 +33,7 @@ return {
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
       default = { "lsp", "path", "snippets", "buffer" },
-      min_keyword_length = 2,
+      cmdline = {},
     },
   },
   opts_extend = { "sources.default" },
