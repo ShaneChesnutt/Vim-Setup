5ac25e61c5fa88db713f1c34e70f60e15d9b5674
Shane Chesnutt <shanechesnutt@gmail.com>
2025-04-16 20:53:04 -0400

    Use whichkey helix layout

--- lua/walnutt/plugins/which-key.lua
+++ lua/walnutt/plugins/which-key.lua
@@ -1,13 +1,8 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
+  opts_extend = { "spec" },
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    preset = "helix",
   },
 }
