28955510f38563585f3a79aab68465bb1e8cda8d
Shane Chesnutt <shanechesnutt@gmail.com>
2025-04-22 21:28:00 -0400

    Add mini pairs, cursorword, and icons

---
+++ lua/walnutt/plugins/mini.lua
@@ -0,0 +1,17 @@
+return {
+  { "echasnovski/mini.icons", version = "*" },
+  {
+    "echasnovski/mini.pairs",
+    version = "*",
+    config = function()
+      require("mini.pairs").setup()
+    end,
+  },
+  {
+    "echasnovski/mini.cursorword",
+    version = "*",
+    config = function()
+      require("mini.cursorword").setup()
+    end,
+  },
+}
