23125687dbea433de9fbb30baee2fa4f92e72062
Shane Chesnutt <shanechesnutt@gmail.com>
2026-02-26 21:00:42 -0500

    Use global line status for lualine

--- lua/walnutt/plugins/lualine.lua
+++ lua/walnutt/plugins/lualine.lua
@@ -15,7 +15,7 @@ return {
         disabled_filetypes = { statusline = {}, winbar = {} },
         ignore_focus = {},
         always_divide_middle = true,
-        globalstatus = false,
+        globalstatus = true,
         refresh = { statusline = 1000, tabline = 1000, winbar = 1000 },
       },
       sections = {
