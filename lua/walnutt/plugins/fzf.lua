b2c1be0b51e4070679c26d362cb5f702868d1373
Shane Chesnutt <shanechesnutt@gmail.com>
2026-02-26 20:41:44 -0500

    Add keymaps to fzf config

--- lua/walnutt/plugins/fzf.lua
+++ lua/walnutt/plugins/fzf.lua
@@ -16,28 +16,46 @@ return {
 
     local keymap = vim.keymap
 
-    keymap.set("n", "<leader>ff", fzf.files, { desc = "Fzf: Find Files" })
+    keymap.set("n", "<leader>ff", fzf.files, { desc = "fzf: Find files" })
+    keymap.set("n", "<leader>fs", fzf.live_grep, { desc = "fzf: Live grep" })
+    keymap.set("n", "<leader>fb", fzf.buffers, { desc = "fzf: Search buffers" })
+    keymap.set(
+      "v",
+      "<leader>fv",
+      fzf.grep_visual,
+      { desc = "fzf: Seach visual selection" }
+    )
+    keymap.set(
+      { "n", "v" },
+      "<leader>fw",
+      fzf.grep_cword,
+      { desc = "fzf: Search word" }
+    )
     keymap.set(
       "n",
-      "<leader>fg",
-      fzf.git_files,
-      { desc = "fzf: Search Git Files" }
+      "<leader>fr",
+      "<cmd>FzfLua grep resume=true<CR>",
+      { desc = "fzf: Resume search" }
     )
-    keymap.set("n", "<leader>fs", fzf.live_grep, { desc = "fzf: Live Grep" })
-    keymap.set("n", "<leader>fb", fzf.buffers, { desc = "fzf: Search Buffers" })
-    keymap.set("n", "<leader>fw", fzf.grep_cword, { desc = "fzf: Search Word" })
-    keymap.set("n", "<leader>gs", fzf.git_status, { desc = "fzf: Git Status" })
+    keymap.set("n", "<leader>gs", fzf.git_status, { desc = "fzf: Git status" })
+    keymap.set("n", "<leader>gf", fzf.git_files, { desc = "fzf: Git files" })
     keymap.set(
       "n",
       "<leader>gc",
       fzf.git_commits,
-      { desc = "fzf: Git Commits" }
+      { desc = "fzf: Git commits" }
     )
     keymap.set(
       "n",
       "<leader>sc",
       fzf.spell_suggest,
-      { desc = "fzf: Spelling Suggestions" }
+      { desc = "fzf: Spelling suggestions" }
+    )
+    keymap.set(
+      "n",
+      "<leader>bl",
+      fzf.blines,
+      { desc = "fzf: Search all buffers" }
     )
   end,
 }
