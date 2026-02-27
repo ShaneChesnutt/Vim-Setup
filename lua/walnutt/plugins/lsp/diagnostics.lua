3df6f5b01f38f8a738e1dabac05a09b7059d9bd7
Shane Chesnutt <shanechesnutt@gmail.com>
2026-02-26 20:53:44 -0500

    Update diagnostics style

--- lua/walnutt/plugins/lsp/diagnostics.lua
+++ lua/walnutt/plugins/lsp/diagnostics.lua
@@ -6,7 +6,25 @@ return {
     config = function()
       require("tiny-inline-diagnostic").setup({
         options = {
+          show_source = {
+            enabled = true,
+          },
           show_all_diags_on_cursorline = true,
+          multilines = {
+            enabled = true,
+          },
+        },
+        signs = {
+          left = "",
+          right = "",
+          diag = "●",
+          arrow = "    ",
+          up_arrow = "    ",
+          vertical = " │",
+          vertical_end = " └",
+        },
+        blend = {
+          factor = 0.22,
         },
       })
       vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
